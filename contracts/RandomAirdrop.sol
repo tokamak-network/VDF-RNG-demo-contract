e98188e19bdd05a763da5775f3a5b9dedd39e5a7
지의신 <64007655+usgeeus@users.noreply.github.com>
2024-01-23 11:25:19 +0900

    Add register function and related variables

--- contracts/RandomAirdrop.sol
+++ contracts/RandomAirdrop.sol
@@ -6,45 +6,58 @@ import "./CommitRevealRecoverRNG/CommitRevealRecoverRNG.sol";
 contract RandomAirdrop is CommitRevealRecoverRNG {
     using BigNumbers for *;
     mapping(address participantAddress => uint256[] rounds) private participatedRounds;
+    mapping(address participantAddress => mapping(uint256 round => uint256 registerIndex))
+        private registerIndexAtRound;
+    mapping(uint256 round => address[] participants) private participantsAtRound;
     uint256 public randomAirdropRound;
 
-    event RandomAirdropEntered(address indexed _entrant, uint256 _timestamp);
+    error RoundNotCompleted();
+    error AlreadyRegistered();
+
+    event RandomAirdropRegisteredAtRound(address indexed _entrant, uint256 _timestamp);
 
     function setUp(
         uint256 _commitDuration,
         uint256 _commitRevealDuration,
         BigNumber calldata _n,
         VDFClaim[] calldata _proofs
-    ) public {
+    ) external override returns (uint256) {
         checkStage(randomAirdropRound);
         if (valuesAtRound[randomAirdropRound].stage != Stages.Finished) revert StageNotFinished();
         uint256 _round = _setUp(_commitDuration, _commitRevealDuration, _n, _proofs);
         randomAirdropRound = _round;
+        return _round;
     }
 
-    function enterEventByCommit(BigNumber memory _c) public {
-        uint256 _round = randomAirdropRound;
-        _commit(_round, _c);
+    function registerNextRound() external {
+        uint256 _round = nextRound;
+        if (registerIndexAtRound[msg.sender][_round] != 0) revert AlreadyRegistered();
+        registerIndexAtRound[msg.sender][_round] = participantsAtRound[_round].length;
+        participantsAtRound[_round].push(msg.sender);
         participatedRounds[msg.sender].push(_round);
-        emit RandomAirdropEntered(msg.sender, block.timestamp);
     }
 
     function getRankPointOfEachParticipants(
         uint256 _round
-    ) public view returns (address[] memory addresses, bytes[] memory rankPoints) {
-        require(valuesAtRound[_round].isCompleted, "round not completed");
+    ) external view returns (address[] memory addresses, bytes[] memory rankPoints) {
+        if (!valuesAtRound[_round].isCompleted) revert RoundNotCompleted();
         BigNumber memory a = abi.encodePacked(keccak256(valuesAtRound[_round].omega.val)).init();
-        addresses = new address[](valuesAtRound[_round].numOfParticipants);
-        rankPoints = new bytes[](valuesAtRound[_round].numOfParticipants);
-        for (uint256 i = 0; i < valuesAtRound[_round].numOfParticipants; i++) {
-            address _address = commitRevealValues[_round][i].participantAddress;
+        uint256 numOfParticipants = participantsAtRound[_round].length;
+        addresses = new address[](numOfParticipants);
+        rankPoints = new bytes[](numOfParticipants);
+        for (uint256 i; i < numOfParticipants; i++) {
+            address _address = participantsAtRound[_round][i];
             addresses[i] = _address;
             bytes memory b = abi.encodePacked(keccak256(abi.encodePacked(_address)));
             rankPoints[i] = a.sub(b.init()).val;
         }
     }
 
-    function getParticipatedRounds() public view returns (uint256[] memory) {
+    function getParticipatedRounds() external view returns (uint256[] memory) {
         return participatedRounds[msg.sender];
+    }
+
+    function getParticipantsAtRound(uint256 _round) external view returns (address[] memory) {
+        return participantsAtRound[_round];
     }
 }
